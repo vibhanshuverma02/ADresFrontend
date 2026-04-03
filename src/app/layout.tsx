da98daf44b59254f3b0dec3cb00803fd8570437d
vibhanshuverma02 <vibhanshuverma.dpsr@gmail.com>
2026-04-03 12:18:53 +0530

    pushed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,12 +23,12 @@ export const metadata: Metadata = {
   // ✅ ADD THIS PART (favicon.io setup)
   icons: {
     icon: [
-      { url: "/favicon.ico" },                 // standard favicon
-      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
-      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/ADRES-logo.png" },                 // standard favicon
+      { url: "/ADRES-logo.png", sizes: "16x16", type: "image/png" },
+      { url: "/ADRES-logo.png", sizes: "32x32", type: "image/png" },
     ],
     apple: [
-      { url: "/apple-touch-icon.png", sizes: "180x180" },
+      { url: "/ADRES-logo.png", sizes: "180x180" },
     ],
   },
 
